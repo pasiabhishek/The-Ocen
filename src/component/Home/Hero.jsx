7b14f2e0ad2d68d26239cb0bf295427901850811
pasi <pasiabhishek6070@gmail.com>
2026-09-20 00:36:31 +0530

    added animation

--- src/component/Home/Hero.jsx
+++ src/component/Home/Hero.jsx
@@ -2,10 +2,43 @@ import React from 'react'
 import Header from '../common/Header'
 import HeroMiddle from '../common/HeroMiddle'
 import { Wave1, Wave2 } from '../common/Wave1'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.to(".middel-hero-gsap ", {
+   
+      x: 1000,
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top ",
+        end: "bottom top",
+        scrub: true,
+        markers: false
+      }
+      
+    })
+
+    gsap.to(".nav",{
+      x:100,
+       scrollTrigger: {
+        trigger: ".hero",
+        start: 0,
+        end: "bottom top",
+        scrub: true,
+        markers: false
+      }
+    })
+
+    
+  }, [])
+
   return (
-    <div className='hero bg-gray-950 flex justify-center relative'>
+    <div className='hero bg-gray-950 flex justify-center relative overflow-hidden h-[100vh]'>
       <Wave2 />
       <video
         autoPlay
@@ -16,14 +49,13 @@ const Hero = () => {
       >
         <source src="/Hero-bg2.mp4" type='video/mp4' />
       </video>
-
-      {/* Overlay */}
-      <div className='absolute inset-0 overlay z-[1]'></div>
-
+      <div className='absolute inset-0 overlay z-1'>
+        <HeroMiddle className="heroMiddle"/>
+      </div>
       <Header />
-      <HeroMiddle />
-      <Wave1 />
 
+
+      <Wave1 />
     </div>
   )
 }
